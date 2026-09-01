996a6e21d8c85e9268292e7d35ecd23031c696cb
Adewale Okunade Christopher <princecph4@gmail.com>
2026-09-01 19:42:38 +0100

    fix: Add DROP TRIGGER IF EXISTS to prevent trigger already exists error

--- database/complete-migration.sql
+++ database/complete-migration.sql
@@ -493,9 +493,11 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
+DROP TRIGGER IF EXISTS update_user_updated_at ON "User";
 CREATE TRIGGER update_user_updated_at BEFORE UPDATE ON "User"
 FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
 
+DROP TRIGGER IF EXISTS update_glossary_updated_at ON "user_glossary";
 CREATE TRIGGER update_glossary_updated_at BEFORE UPDATE ON "user_glossary"
 FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
 
